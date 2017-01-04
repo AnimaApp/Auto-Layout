746ba5e5bccaa3e1bc3eb82d01d7028904f6878d
Or Arbel <orarbel@gmail.com>
2017-01-04 13:09:30 -0500

    0.0.4

--- AutoLayoutPlugin.sketchplugin/Contents/Resources/AutoLayoutPlugin.bundle/Contents/Resources/ui.js
+++
@@ -1,9 +0,0 @@
-
-var createAlert = function(message, informativeText, customize) {
-  var alert = [[NSAlert alloc] init]
-  [alert setMessageText:message]
-  if (informativeText) {
-    [alert setInformativeText:informativeText]
-  }
-  return alert
-};
